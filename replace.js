33af0df4045ad056d69416382c9259abc21dff6c
Erevney <75116258+Erevney@users.noreply.github.com>
2022-06-11 20:48:24 -0500

    Delete replace.js

--- replace.js
+++
@@ -1,5 +0,0 @@
-//El método replace() devuelve una nueva cadena con los valores reemplazados.
-//en este caso remplaza esta oracion por otra
-
-let text = "ジョジョの 奇妙な冒険";
-let result = text.replace("ジョジョの 奇妙な冒険", "JoJo's Bizarre Adventure");
