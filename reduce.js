2516166aaa73ed3e96e2cc73685fad055a0f38e5
Erevney <75116258+Erevney@users.noreply.github.com>
2022-06-11 20:11:08 -0500

    Delete reduce.js

--- reduce.js
+++
@@ -1,9 +0,0 @@
-//El método reduce() ejecuta una función reductora sobre cada elemento de un array, devolviendo como resultado un único valor.
-//en este caso resta todos los numeros
-const numeros = [13, 57, 91];
-
-document.getElementById("demo").innerHTML = numeros.reducir(Funci);
-
-function Funci(total, num) {
-  return total - num;
-}
